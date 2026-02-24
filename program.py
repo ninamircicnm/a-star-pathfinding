4f220ae0d24f2f794d0b05429bd602262fa514d4
ninamircicnm <ninamircic.nm@gmail.com>
2026-02-24 19:30:17 +0100

    Add Tkinter canvas

--- program.py
+++ program.py
@@ -1,5 +1,8 @@
 import heapq
+import tkinter as tk
+from tkinter import simpledialog, messagebox
 
+# A* algorithm implementation
 def astar(graph, heuristic, start, goal):
 
     open_set=[]
@@ -21,3 +24,27 @@ def astar(graph, heuristic, start, goal):
                 new_f = new_g + heuristic.get(neighbor,0)
                 heapq.heappush(open_set, (new_f, new_g, neighbor, path + [neighbor]))
     return None, float('inf')
+
+# GUI - Tkinter
+
+class GraphPlannerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("A* Algorithm")
+        self.canvas = tk.Canvas(root, width=700, height=500, bg="white")
+        self.canvas.pack()
+
+        self.nodes = {}
+        self.graph = {}
+        self.heuristic = {}
+        self.heuristic_texts = {}
+
+        self.node_counter = 0
+        self.selected_node = None
+        self.start_node = None
+        self.goal_node = None
+
+        frame = tk.Frame(root)
+        frame.pack(pady=5)
+
+        self.canvas.bind("<Button-1>", self.on_canvas_click)
